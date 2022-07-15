6b3acd8eadf944a371178eb395f861ef5be94daf
benekker <benekker1@gmail.com>
2022-07-15 13:48:59 -0500

    Modified Super Model to Include catchphrase and super_type

--- supers/models.py
+++ supers/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 
-class Super(models.model):
+from super_types.models import SuperType
+
+class Super(models.Model):
     name = models.CharField(max_length=100)
     alter_ego = models.CharField(max_length=100)
     primary_ability = models.CharField(max_length=150)
     secondary_ability = models.CharField(max_length=150)
+    catchphrase = models.CharField(max_length=200)
+    super_type = models.ForeignKey(SuperType, on_delete=models.CASCADE)
