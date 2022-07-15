72bdda840563afb6d6a8bd5beab7104c53a4c913
benekker <benekker1@gmail.com>
2022-07-15 15:06:36 -0500

    Added Untracked Files

--- supers/views.py
+++ supers/views.py
@@ -1,11 +1,15 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Super
-from .serailizers import SuperSerializer
+from .serializers import SuperSerializer
 from rest_framework import status
 from constants import GET, POST, PUT, DELETE
 
 
+@api_view([GET, POST])
+def supers_list(request):
 
+    if request.method == GET:
+        supers = Super.objects.filter()
 
 
