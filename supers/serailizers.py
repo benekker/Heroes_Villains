3a567b5d22945047a442f2f5ac5d40771393cb02
benekker <benekker1@gmail.com>
2022-07-15 15:05:34 -0500

    Fixed a Typo in File Name

--- supers/serailizers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Super
-
-class SuperSerializer(serializers.SuperSerializer):
-    class Meta:
-        model = Super
-        fields = ['name', 'alter_ego', 'primary_ability', 'secondary_ability', 'catachphrase', 'super_type']
