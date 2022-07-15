49a3b1b446808f62a8d06509fa4b15bfdc424b7f
benekker <benekker1@gmail.com>
2022-07-15 13:44:16 -0500

    Created Super Model + Registered SuperUser for Admin Capabilities

--- supers/models.py
+++ supers/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Super(models.model):
+    name = models.CharField(max_length=100)
+    alter_ego = models.CharField(max_length=100)
+    primary_ability = models.CharField(max_length=150)
+    secondary_ability = models.CharField(max_length=150)
