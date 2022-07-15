056dca9c8f2208b2d564dd04dcbef7251f595463
benekker <benekker1@gmail.com>
2022-07-15 14:27:40 -0500

    Created Constants File for CRUD Operations to Import into Views File

--- supers/views.py
+++ supers/views.py
@@ -1,5 +1,11 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Super
+from .serailizers import SuperSerializer
+from rest_framework import status
+from constants import GET, POST, PUT, DELETE
+
+
+
 
 
